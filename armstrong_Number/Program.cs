a7584fb8320f553eedea537f83984e2c7243e8d1
suhas <suhas@DELL>
2024-12-03 15:53:07 +0530

    Add project files.

---
+++ armstrong_Number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace armstrong_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,sum=0;
+            Console.WriteLine("Enter the any Number");
+            n=int.Parse(Console.ReadLine());
+            int tmp = n;
+            while (n > 0)
+            {
+                int r = n % 10;
+                sum = sum + r * r * r;
+                n = n / 10;
+            }
+            if(sum==tmp)
+            {
+                Console.WriteLine("Armstrong Number");
+            }
+            else
+            {
+                Console.WriteLine("Not Armstrong Number");
+            }
+            Console.ReadKey();
+        }
+    }
+}
